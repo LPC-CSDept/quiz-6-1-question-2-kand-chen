4b69332ff1053c4df967f2c5502443696d29c064
kand-chen <ilovegreen109@gmail.com>
2022-10-09 17:45:17 -0700

    quiz q2 push 2

--- q2.cpp
+++ q2.cpp
@@ -14,13 +14,25 @@ int  findMin(int n1, int n2, int n3);
 void printResult(int n1, int n2, int n3, int min);
 
 int main(){
-	
+	int a, b, c, min;
+	getInput(a, b, c);
+	min = findMin(a, b, c);
+	printResult(a, b, c, min);
 }
 
 void getInput(int &n1, int &n2, int &n3){
 	cout << "Enter three values, separated by a space: " << endl;
 	cin >> n1 >> n2 >> n3; 
 }
+
+int  findMin(int n1, int n2, int n3){
+	int miN;
+	miN = n1;
+	for (int i = 0; i < 3; i++){
+		if 
+	}
+}
+
 
 void printResult(int n1, int n2, int n3, int min)
 {
