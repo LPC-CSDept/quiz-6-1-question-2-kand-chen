87d8ccf4dea41c94e591a3ee0684a256895b9f24
kand-chen <ilovegreen109@gmail.com>
2022-10-09 17:42:02 -0700

    quiz Q2

--- q2.cpp
+++ q2.cpp
@@ -8,7 +8,22 @@ using namespace std;
 
 // ******************************
 // this function will help you pass the test. When you print the result with this function, you will get the same output format
+
+void getInput(int &n1, int &n2, int &n3);
+int  findMin(int n1, int n2, int n3);
+void printResult(int n1, int n2, int n3, int min);
+
+int main(){
+	
+}
+
+void getInput(int &n1, int &n2, int &n3){
+	cout << "Enter three values, separated by a space: " << endl;
+	cin >> n1 >> n2 >> n3; 
+}
+
 void printResult(int n1, int n2, int n3, int min)
 {
 	cout << n1 << " " << n2 << " " << n3 << " " << min << " " << endl;
 }
+
